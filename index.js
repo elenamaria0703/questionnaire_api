c3f02d0b9db15a1e04d4b1ebacb80b3c30ac96cb
Maria Elena <mariaelena07@yahoo.com>
2021-02-13 21:16:30 +0200

    routes for databases

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express= require('express');
-const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv/config');
 
 app.use(cors());
 app.use(bodyParser.json());
 const databasesRoute = require('./routes/databeses');
 app.use('/databases', databasesRoute);
 
-mongoose.connect(process.env.DB_CONNECT_STRING
-    ,{ useNewUrlParser: true , useUnifiedTopology: true}, () =>
-    console.log("connected db")
-);
 
 
 app.listen(3000);
--- routes/databeses.js
+++ routes/databeses.js
@@ -1,8 +1,48 @@
 const express= require('express');
 const router = express.Router();
+const fs = require('fs') 
+const MongoClient = require('mongodb').MongoClient;
 
-router.get('/', (req,res) =>{
-    res.send("Hello world!")
+require('dotenv/config');
+
+const uri = process.env.DB_CONNECT_STRING;
+let client;
+async function connectDatabase(){
+    client = await MongoClient.connect(uri, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    });
+}
+
+connectDatabase();
+
+router.post('/:id', async(req,res) =>{
+    const id= req.params.id
+    const parent = req.body.parent
+    const userValue= req.body.userValue
+    const db = client.db('licenta');
+    let item = await db.collection('databases').findOne({_id: id});
+    if(item == null && parent !== ''){
+        item = await db.collection('databases').findOne({_id: `${parent}_${id}`});
+    }
+    if(item && item.command && userValue){
+        item.command = item.command.replace('user_value',userValue)
+    }
+    res.send(item)
+});
+
+router.patch('/download', async(req,res)=>{
+    const commands= req.body.commands.join("\n");
+    fs.writeFile('Dockerfile', commands, (err) => { 
+      
+        // In case of a error throw err. 
+        if (err) throw err; 
+    }) 
+    res.download('./Dockerfile', 'Dockerfile')
+    res.end()
 });
 
 module.exports = router;
+
+
+
